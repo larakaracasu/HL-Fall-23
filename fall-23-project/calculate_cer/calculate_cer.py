bfa99cd60514f09e15c106dd22ddfdd6bddc1909
Lara Karacasu <larakaracasu@gmail.com>
2023-11-02 03:37:57 -0400

    Working Manual CER script; in-progress OpenAI script.

--- fall-23-project/calculate_cer/calculate_cer.py
+++ fall-23-project/calculate_cer/calculate_cer.py
@@ -1,42 +1,58 @@
 import os
 import editdistance
 import time
+import nltk
+#nltk.download('punkt')
 
 def calculate_cer(reference, hypothesis):
-    """
-    Calculate Character Error Rate between two strings.
-    """
+    # Calculate Character Error Rate between two strings.
     distance = editdistance.eval(reference, hypothesis)
     return distance / len(reference)
 
+def get_tokens_and_characters(text):
+    # Calculate the number of tokens and characters in the text.
+    tokens = len(nltk.word_tokenize(text))
+    characters = len(text)
+    return tokens, characters
+
 def write_to_file(filename, data):
-    """
-    Write the given data to a file.
-    """
+    # Write the given data to a file.
     with open(filename, 'w', encoding="utf-8") as f:
         f.write(data)
 
 def main():
     ocr_dir = "C:\\Users\\larak\\OneDrive\\Documents\\History-Lab\\ocr_data\\preprocessed_ocr"
     truth_dir = "C:\\Users\\larak\\OneDrive\\Documents\\History-Lab\\truth_data\\preprocessed_truth"
-    results_file = "C:\\Users\\larak\\OneDrive\\Documents\\GitHub\\HL-Fall-23\\fall-23-project\\calculate_cer\\cer_results.txt"
+    results_file = "C:\\Users\\larak\\OneDrive\\Documents\\GitHub\\HL-Fall-23\\fall-23-project\\calculate_cer\\cer_results_manual.txt"
     
     ocr_files = os.listdir(ocr_dir)
     
     total_cer = 0
+    total_tokens_ocr = 0
+    total_characters_ocr = 0
+    total_tokens_truth = 0
+    total_characters_truth = 0
     num_files = len(ocr_files)
 
-    results_data = "Document Name\tCharacter Error Rate\tTime (seconds)\n"
-    results_data += "-"*70 + "\n"
+    results_data = "Document\tCharacter Error Rate\tTokens (OCR)\tCharacters (OCR)\tTokens (Truth)\tCharacters (Truth)\tTime to Process\n"
+    results_data += "-"*100 + "\n"
     
     global_start_time = time.time()  # Start the timer for all documents
 
     for ocr_file in ocr_files:
         with open(os.path.join(ocr_dir, ocr_file), 'r', encoding="utf-8") as f_ocr, \
-             open(os.path.join(truth_dir, ocr_file), 'r', encoding="utf-8") as f_truth:
-            
+            open(os.path.join(truth_dir, ocr_file), 'r', encoding="utf-8") as f_truth:
+
             ocr_content = f_ocr.read()
             truth_content = f_truth.read()
+
+            tokens_ocr, characters_ocr = get_tokens_and_characters(ocr_content)
+            tokens_truth, characters_truth = get_tokens_and_characters(truth_content)  # Calculate tokens and characters for Truth
+
+            total_tokens_ocr += tokens_ocr
+            total_characters_ocr += characters_ocr
+            total_tokens_truth += tokens_truth  # Update the total tokens for Truth
+            total_characters_truth += characters_truth  # Update the total characters for Truth
 
             start_time = time.time()
             cer = calculate_cer(truth_content, ocr_content)
@@ -44,15 +60,24 @@ def main():
 
             elapsed_time = end_time - start_time
             total_cer += cer
-            results_data += f"{ocr_file}\t{cer:.4f}\t{elapsed_time:.4f}\n"
+
+            # Using string formatting to ensure consistent spacing
+            results_data += f"{ocr_file}\t{cer:.4f}\t{tokens_ocr}\t{characters_ocr}\t{tokens_truth}\t{characters_truth}\t{elapsed_time:.4f}\n"
+
+
+
 
     global_end_time = time.time()  # End the timer for all documents
     total_time = global_end_time - global_start_time
     average_cer = total_cer / num_files
 
-    results_data += "\n" + "-"*70 + "\n"
+    results_data += "\n" + "-"*100 + "\n"
     results_data += f"Average Character Error Rate for all documents: {average_cer:.4f}\n"
     results_data += f"Total time to process all documents: {total_time:.4f} seconds\n"
+    results_data += f"Total tokens (OCR) for all documents: {total_tokens_ocr}\n"
+    results_data += f"Total characters (OCR) for all documents: {total_characters_ocr}\n"
+    results_data += f"Total tokens (Truth) for all documents: {total_tokens_truth}\n"
+    results_data += f"Total characters (Truth) for all documents: {total_characters_truth}\n"
 
     # Write the results to a file
     write_to_file(results_file, results_data)
--- fall-23-project/calculate_cer/calculate_cer_chatgpt.py
+++ fall-23-project/calculate_cer/calculate_cer_chatgpt.py
@@ -7,22 +7,26 @@ openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 def get_cer_from_chatgpt(reference, hypothesis):
     """
-    Get Character Error Rate from ChatGPT.
+    Get Character Error Rate from ChatGPT with truncation.
     """
+    # Truncate reference and hypothesis to 2000 characters each
+    reference = reference[:2000]
+    hypothesis = hypothesis[:2000]
+
     prompt = (f"Given the reference text \"{reference}\" and the hypothesis text \"{hypothesis}\", "
               "please compute and provide the Character Error Rate (CER) as a single numerical value. "
               "The Character Error Rate (CER) is calculated as (Substitutions + Insertions + Deletions) / Total number of reference characters. "
               "Ensure the answer is a single number without any additional text.")
+    
     response = openai.Completion.create(model="text-davinci-003", prompt=prompt, max_tokens=1, n=1, temperature=0.1)
     
     try:
         cer = float(response.choices[0].text.strip())
     except ValueError:
         print(f"Unexpected response from ChatGPT: {response.choices[0].text.strip()}")
-        cer = 1.0  # you can default to maximum error or any other fallback strategy
+        cer = 1.0  # Default to the maximum error
     
     return cer
-
 
 def write_to_file(filename, data):
     """
@@ -39,10 +43,12 @@ def main():
     ocr_files = os.listdir(ocr_dir)
     
     total_cer = 0
+    total_tokens_ref = 0
+    total_tokens_hyp = 0
     num_files = len(ocr_files)
 
-    results_data = "Document Name\tCharacter Error Rate\tTime (seconds)\n"
-    results_data += "-"*70 + "\n"
+    results_data = "Document Name\tCharacter Error Rate\tTokens (Reference)\tTokens (Hypothesis)\tTime (seconds)\n"
+    results_data += "-"*100 + "\n"
     
     global_start_time = time.time()
 
@@ -53,21 +59,30 @@ def main():
             ocr_content = f_ocr.read()
             truth_content = f_truth.read()
 
+            num_tokens_ref = len(truth_content.split())
+            num_tokens_hyp = len(ocr_content.split())
+
             start_time = time.time()
             cer = get_cer_from_chatgpt(truth_content, ocr_content)
             end_time = time.time()
 
             elapsed_time = end_time - start_time
             total_cer += cer
-            results_data += f"{ocr_file}\t{cer:.4f}\t{elapsed_time:.4f}\n"
+            total_tokens_ref += num_tokens_ref
+            total_tokens_hyp += num_tokens_hyp
+
+            # Using string formatting to ensure consistent spacing
+            results_data += f"{ocr_file}\t{cer:.4f}\t{num_tokens_ref}\t{num_tokens_hyp}\t{elapsed_time:.4f}\n"
 
     global_end_time = time.time()
     total_time = global_end_time - global_start_time
     average_cer = total_cer / num_files
 
-    results_data += "\n" + "-"*70 + "\n"
+    results_data += "\n" + "-"*100 + "\n"
     results_data += f"Average Character Error Rate for all documents: {average_cer:.4f}\n"
     results_data += f"Total time to process all documents: {total_time:.4f} seconds\n"
+    results_data += f"Total tokens for Reference across all documents: {total_tokens_ref}\n"
+    results_data += f"Total tokens for Hypothesis across all documents: {total_tokens_hyp}\n"
 
     # Write the results to a file
     write_to_file(results_file, results_data)
